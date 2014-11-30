dc360505b97ac4f43497903d8e04e5156934b28d
Tobias Preuss <tobias.preuss@googlemail.com>
2014-11-30 21:35:20 +0100

    Active Jackson parser.

--- src/main/java/info/metadude/java/library/halfnarp/ApiModule.java
+++ src/main/java/info/metadude/java/library/halfnarp/ApiModule.java
@@ -1,12 +1,23 @@
 package info.metadude.java.library.halfnarp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import retrofit.RestAdapter;
+import retrofit.converter.JacksonConverter;
 
 public class ApiModule {
+
+    private static ObjectMapper provideObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(
+                PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+        return objectMapper;
+    }
 
     public static TalkPreferencesService getTalkPreferencesService() {
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint("http://halfnarp.events.ccc.de")
+                .setConverter(new JacksonConverter(provideObjectMapper()))
                 .setLogLevel(RestAdapter.LogLevel.HEADERS)
                 .build();
         return restAdapter.create(TalkPreferencesService.class);
