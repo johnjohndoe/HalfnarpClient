8d7a26c839046d7bd241049e69106f5c91fa218a
Tobias Preuss <tobias.preuss@googlemail.com>
2015-10-27 15:02:03 +0100

    Allow intercepting the HTTP client.

--- src/main/java/info/metadude/java/library/halfnarp/ApiModule.java
+++ src/main/java/info/metadude/java/library/halfnarp/ApiModule.java
@@ -2,20 +2,29 @@ package info.metadude.java.library.halfnarp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
-
 import retrofit.JacksonConverterFactory;
 import retrofit.Retrofit;
+
+import java.util.List;
 
 public final class ApiModule {
 
     public static TalkPreferencesService getTalkPreferencesService() {
-        return createRetrofit(TalkPreferencesService.BASE_URL)
+        return getTalkPreferencesService(null);
+    }
+
+    public static TalkPreferencesService getTalkPreferencesService(final List<Interceptor> httpClientInterceptors) {
+        return createRetrofit(TalkPreferencesService.BASE_URL, httpClientInterceptors)
                 .create(TalkPreferencesService.class);
     }
 
-    private static Retrofit createRetrofit(final String baseUrl) {
+    private static Retrofit createRetrofit(final String baseUrl, final List<Interceptor> httpClientInterceptors) {
         OkHttpClient httpClient = new OkHttpClient();
+        if (httpClientInterceptors != null) {
+            httpClient.interceptors().addAll(httpClientInterceptors);
+        }
 
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
